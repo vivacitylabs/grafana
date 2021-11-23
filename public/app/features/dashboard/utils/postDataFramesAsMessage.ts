4eaed58a29a814adf9994c1bde484bc654a59a01
Sam Williams <sam.williams@vivacitylabs.com>
2021-11-23 12:36:01 +0000

    Add targetDomain var

--- public/app/features/dashboard/utils/postDataFramesAsMessage.ts
+++ public/app/features/dashboard/utils/postDataFramesAsMessage.ts
@@ -2,21 +2,25 @@ import { PanelData, DataFrame, Field } from '@grafana/data';
 import { locationService } from '@grafana/runtime';
 import { UrlQueryMap, UrlQueryValue } from '../../../../../packages/grafana-data/src';
 
-const DOWNLOAD_ID_KEY = 'var-downloadId';
+const DOWNLOAD_ID_KEY = 'downloadId';
+const MESSAGE_TARGET_DOMAIN_KEY = 'messageTargetDomain';
 interface IframeMessage {
   downloadId: UrlQueryValue;
   series: DataFrame[];
 }
 
 export const postDataFramesAsMessage = (data: PanelData) => {
-  const downloadIdVariable = getDownloadIdVariable();
+  const variables: UrlQueryMap = locationService.getSearchObject();
+  const downloadIdVariable = getDownloadIdVariable(variables);
+  const messageTargetDomain = getTargetDomainVariable(variables);
   if (window.parent && downloadIdVariable) {
     const series = cleanDataOfUnsendableProperties(data);
     const iframeMessage: IframeMessage = {
       downloadId: downloadIdVariable,
       series: series,
     };
-    window.parent.postMessage(iframeMessage);
+    const targetOrigin = messageTargetDomain ? `https://${messageTargetDomain}` : '*';
+    window.parent.postMessage(iframeMessage, targetOrigin);
   }
 };
 
@@ -32,7 +36,10 @@ const cleanDataOfUnsendableProperties = (data: PanelData): DataFrame[] => {
   return series;
 };
 
-const getDownloadIdVariable = (): UrlQueryValue | undefined => {
-  const variables: UrlQueryMap = locationService.getSearchObject();
+const getDownloadIdVariable = (variables: UrlQueryMap): UrlQueryValue | undefined => {
   return variables[DOWNLOAD_ID_KEY];
+};
+
+const getTargetDomainVariable = (variables: UrlQueryMap): UrlQueryValue | undefined => {
+  return variables[MESSAGE_TARGET_DOMAIN_KEY];
 };
